027237b17ca30badcf2dc69c551dbd31b5d81767
Christoph Wurst <christoph@winzerhof-wurst.at>
2024-02-13 18:50:22 +0100

    feat: comparison with empty passwords
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # authtoken-index-bench
 
+## Baseline
+
 ```bash
 mariadb-slap -u root -p --delimiter=";" --create=schema_baseline.sql --query=client_baseline.sql --concurrency=10 --iterations=5
-mariadb-slap -u root -p --delimiter=";" --create=schema_new.sql --query=client.sql --concurrency=10 --iterations=5
+```
+
+## Empty passwords
+
+```bash
+mariadb-slap -u root -p --delimiter=";" --create=schema_empty-password.sql --query=client_baseline.sql --concurrency=10 --iterations=5
 ```
---
+++ schema_empty-password.sql
@@ -0,0 +1,48 @@
+CREATE TABLE `oc_authtoken` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uid` varchar(64) NOT NULL DEFAULT '',
+  `login_name` varchar(255) NOT NULL DEFAULT '',
+  `password` longtext DEFAULT NULL,
+  `name` longtext NOT NULL,
+  `token` varchar(200) NOT NULL DEFAULT '',
+  `type` smallint(5) unsigned DEFAULT 0,
+  `remember` smallint(5) unsigned DEFAULT 0,
+  `last_activity` int(10) unsigned DEFAULT 0,
+  `last_check` int(10) unsigned DEFAULT 0,
+  `scope` longtext DEFAULT NULL,
+  `expires` int(10) unsigned DEFAULT NULL,
+  `private_key` longtext DEFAULT NULL,
+  `public_key` longtext DEFAULT NULL,
+  `version` smallint(5) unsigned NOT NULL DEFAULT 1,
+  `password_invalid` tinyint(1) DEFAULT 0,
+  `password_hash` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `authtoken_token_index` (`token`),
+  KEY `authtoken_uid_index` (`uid`),
+  KEY `authtoken_last_activity` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
+
+INSERT INTO oc_authtoken (
+  id,
+  uid,
+  login_name,
+  last_activity,
+  name,
+  token
+)
+SELECT
+  n,
+  CONCAT('user', n),
+  CONCAT('User', n),
+  UNIX_TIMESTAMP(),
+  'Test token',
+  UUID()
+  FROM
+(
+select a.N + b.N * 10 + c.N * 100 + d.N * 1000 + e.N * 10000 + 1 N
+from (select 0 as N union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) a
+      , (select 0 as N union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) b
+      , (select 0 as N union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) c
+      , (select 0 as N union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) d
+      , (select 0 as N union all select 1 union all select 2 union all select 3 union all select 4 union all select 5 union all select 6 union all select 7 union all select 8 union all select 9) e
+) t;
