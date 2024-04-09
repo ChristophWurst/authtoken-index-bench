4c2e40261e771fb8b0dbb84c394ede7be25acfe2
Christoph Wurst <christoph@winzerhof-wurst.at>
2024-04-09 09:34:58 +0200

    fix: add missing baseline client
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ client_baseline.sql
@@ -0,0 +1,10 @@
+SELECT * FROM oc_authtoken WHERE id = FLOOR(RAND()*100000);
+UPDATE oc_authtoken SET last_activity=UNIX_TIMESTAMP() WHERE id=FLOOR(RAND()*100000);
+SELECT * FROM oc_authtoken WHERE id = FLOOR(RAND()*100000);
+SELECT * FROM oc_authtoken WHERE id = FLOOR(RAND()*100000);
+SELECT * FROM oc_authtoken WHERE id = FLOOR(RAND()*100000);
+SELECT * FROM oc_authtoken WHERE id = FLOOR(RAND()*100000);
+SELECT * FROM oc_authtoken WHERE id = FLOOR(RAND()*100000);
+SELECT * FROM oc_authtoken WHERE id = FLOOR(RAND()*100000);
+SELECT * FROM oc_authtoken WHERE id = FLOOR(RAND()*100000);
+SELECT * FROM oc_authtoken WHERE id = FLOOR(RAND()*100000);
